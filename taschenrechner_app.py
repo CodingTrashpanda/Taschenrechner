ef2c75f7678d0f49d4200f84a4f65ad6052568f7
Titas Siauciunas <siau.titas.ciunas@gmail.com>
2023-01-27 17:38:47 +0100

    Division durch 0 nicht mehr möglich.

--- taschenrechner_app.py
+++ taschenrechner_app.py
@@ -1,57 +1,61 @@
-def add (a, b):
-    answer = a + b
-    print(str(a) + " + " + str(b) + " = " + str(answer) + "\n")
-    with open("result.txt", "a") as f:
-        f.write(f"{a} + {b} = {answer}\n")
+from typing import Union
 
-def sub(a, b):
-    answer = a - b
-    print(str(a) + " - " + str(b) + " = " + str(answer) + "\n")
-    with open("result.txt", "a") as f:
-        f.write(f"{a} - {b} = {answer}\n")
+
+def add(a:int, b:int)->int:
+    return a+b
+
+def sub(a:int, b:int)->int:
+    return a-b
     
-def mul(a, b):
-    answer = a * b
-    print(str(a) + " * " + str(b) + " = " + str(answer) + "\n")
-    with open("result.txt", "a") as f:
-        f.write(f"{a} * {b} = {answer}\n")
-    
-def div(a, b):
-    answer = a / b
-    print(str(a) + " / " + str(b) + " = " + str(answer) + "\n")
-    with open("result.txt", "a") as f:
-        f.write(f"{a} / {b} = {answer}\n")
-    
-    
+def mul(a:int, b:int)->int:
+    return a*b
+
+def div(a:int, b:int)-> Union[int,float,None]:
+    try:
+        return a/b
+    except ZeroDivisionError:
+        return None
+
+def log_result(function, a, b):
+    result = function(a, b)
+    if result != None:
+        res = f"{a} {function.__name__} {b} = {result}\n"
+        print(res)
+        with open("result.txt", "a") as f:
+            f.write(res)
+    else:
+        if function == div:
+            print("division by zero is not allowed")
+
 while True:
     print("A. Addition")
     print("B. Subtraction")
     print("C. Multiplication")
     print("D. Division")
     print("E. Exit")
-
+    
     choice = input("Enter your choice: ")
-
+    
     if choice == "A" or choice == "a":
         print("Addition")
         a = int(input("Enter first number: "))
         b = int(input("Enter second number: "))
-        add(a, b)
+        log_result(add, a, b)
     elif choice == "B" or choice == "b":
         print("Subtraction")
         a = int(input("Enter first number: "))
         b = int(input("Enter second number: "))
-        sub(a, b)
+        log_result(sub, a, b)
     elif choice == "C" or choice == "c":
         print("Multiplication")
         a = int(input("Enter first number: "))
         b = int(input("Enter second number: "))
-        mul(a, b)
+        log_result(mul, a, b)
     elif choice == "D" or choice == "d":
         print("Division")
         a = int(input("Enter first number: "))
         b = int(input("Enter second number: "))
-        div(a, b)
+        log_result(div, a, b)
     elif choice == "E" or choice == "e":
         print("Program Ended")
         quit()
